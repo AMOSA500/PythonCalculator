6a0508b47311df2b9f4a8ca36252153a0472d753
AMOSA500 <66817828+AMOSA500@users.noreply.github.com>
2023-11-16 23:46:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from art import logo
+
+print(logo)
+
+def add(n1,n2):
+  return n1 + n2
+
+def subtract(n1,n2):
+  return n1 - n2
+
+def multiply(n1,n2):
+  return n1 * n2
+
+def divide(n1,n2):
+  return round(n1 / n2, 2)
+  
+
+# Create a dictionary
+operations = {
+  "+": add,
+  "-":subtract,
+  "*":multiply,
+  "/":divide 
+}
+
+while True:
+  try:
+    num1 = int(input("What is your first number: "))
+    num2 = int(input("What is your second number: "))
+    s_no = 0
+    for symbols in operations:
+      s_no += 1
+      print(f"{s_no}. {symbols}")
+    operation_symbol = input("Enter your operation symbol: ")
+    if operation_symbol in operations:
+      calculator_function = operations[operation_symbol]
+      answer = calculator_function(num1, num2)
+  
+      print(f"The {operation_symbol} of {num1} and {num2} = {answer}")
+      print("------RESTART-------")
+      restart = int(input("1. Restart\n2. Quit\n"))
+      if restart == 2:
+        print("Thank you and GoodBye")
+        break    
+    else:
+      print("Operator cannot be found")
+  except ValueError as e:
+    print(f"Integers only - {e}")
